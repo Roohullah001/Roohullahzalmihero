b7dbd7324e5392f1c5d22f3ca7b730fda617e54c
Roohullah001 <83687211+Roohullah001@users.noreply.github.com>
2021-05-05 21:47:20 +0300

    Update Roohullahzalmifighterhero.py
    git clone https://github.com/Roohullah001/Roohullahzalmihero.git
    ls
    cd Roohullahzalmihero
    ls
    python2 Roohullahzalmifighterhero.py

--- Roohullahzalmifighterhero.py
+++ Roohullahzalmifighterhero.py
@@ -16,8 +16,8 @@ sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-br.addheaders = [('User-Agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; Xbox; Xbox One)Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; Xbox; Xbox One)]
-
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.11')]
+ 
 def keluar():
 	print "\x1b[1;91mExit"
 	os.sys.exit()
